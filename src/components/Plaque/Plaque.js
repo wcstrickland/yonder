12f0c47cdd408d30bce579a2d4035da698ecdb75
bill <wcstrickland1@gmail.com>
2023-03-24 14:18:55 -0500

    stuff

--- src/components/Plaque/Plaque.js
+++ src/components/Plaque/Plaque.js
@@ -5,55 +5,86 @@ import './Plaque.css';
 export default function Plaque(props) {
 
     const [hp, setHp] = useState(props.hp)
-    const [statusHidden, setStatusHidden] = useState(true)
+    const [init, setInit] = useState(props.init)
+    const [conditions, setConditions] = useState({ ...props.conditions })
+    const [dead, setDead] = useState(false)
 
-    function handleOnMouse() {
-        setStatusHidden(false)
+
+
+    function updatePlaque(cond, val) {
+        let nwPlq = {
+            "name": props.name,
+            "ac": props.ac,
+            "hp": hp,
+            "init": init,
+            "id": props.id,
+            "conditions": { ...conditions }
+        }
+        props.updateFunc(nwPlq)
     }
 
-    function handleOffMouse() {
-        setStatusHidden(true)
+    function rndrConditions() {
+        let outputArr = []
+        for (let i = 0; i < Object.keys(conditions).length; i++) {
+            let addr = {}
+            addr[Object.keys(conditions)[i]] = Object.values(conditions)[i]
+            outputArr.push(addr)
+        }
+        return (
+            outputArr.map(c => <div style={{ display: "flex" }}><input type={"checkbox"} id={Object.keys(c)[0]} name={Object.keys(c)[0]} ></input><label  >{Object.keys(c)[0]}</label></div>)
+        )
+    }
+
+    if(!dead){
+        if(hp === "0")setDead(true)
+    }
+    else{
+        if(hp !== "0")setDead(false)
     }
 
     return (
         <>
-            <article className='plaque' onMouseOver={handleOnMouse} onMouseOut={handleOffMouse}>
-                <div onClick={() => props.removeFunc()} id={"closeButton"}>x</div>
-                <div className='charNums'>
-                    <input id="hp" type="text" placeholder={props.hp} onChange={(e) => setHp(e.target.value)} />
-                    <input id="ac" type="text" value={props.ac} />
-                    <input id="init" type="text" value={props.init} />
-                    <div className={statusHidden ? "plaqueDetail" : "plaqueDetailHidden"}>
+            {!dead 
+            ? 
+            <article className="plaque" >
+                    <div>
                         <div id="name">{props.name}</div>
                     </div>
-                </div>
-                <ul className={statusHidden ? "hiddenStatus" : "shownStatus"}>
-                    <div style={{ display: "flex" }}>
-                        <input type={"checkbox"} id="poisoned" name="poisoned"></input>
-                        <label htmlFor='poisoned'>poisoned</label>
+                    <div className='charNums'>
+                        <input id="hp" type="text" placeholder={props.hp} onChange={(e) => {
+                            setHp(e.target.value)
+                        }} />
+                        <input id="ac" type="text" value={props.ac} />
+                        <input id="init" type="text" value={props.init}/>
                     </div>
-                    <div style={{ display: "flex" }}>
-                        <input type={"checkbox"} id="frightened" name="frightened"></input>
-                        <label htmlFor='frightened'>frightened</label>
-                    </div>
-                    <div style={{ display: "flex" }}>
-                        <input type={"checkbox"} id="paralyzed" name="paralyzed"></input>
-                        <label htmlFor='paralyzed'>paralyzed</label>
-                    </div>
-                    <div style={{ display: "flex" }}>
-                        <input type={"checkbox"} id="restrained" name="restrained"></input>
-                        <label htmlFor='restrained'>restrained</label>
-                    </div>
-                    <div style={{ display: "flex" }}>
-                        <input type={"checkbox"} id="charmed" name="charmed"></input>
-                        <label htmlFor='charmed'>charmed</label>
-                    </div>
-                    <div style={{ display: "flex" }}>
-                        <input type={"checkbox"} id="petrified" name="petrified"></input>
-                        <label htmlFor='petrified'>petrified</label>
-                    </div>
-                </ul>
+                    <ul className="shownStatus">
+                        {rndrConditions()}
+                    </ul>
             </article>
+            : 
+            <article className="plaque" >
+                    <div>
+                        <div id="name">{props.name}</div>
+                    </div>
+                    <div className='charNums'>
+                        <input id="hp" type="text" placeholder={props.hp} onChange={(e) => {
+                            setHp(e.target.value)
+                        }} />
+                        <input id="ac" type="text" value={props.ac} />
+                        <input id="init" type="text" value={props.init} />
+                    </div>
+                    <div style={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr 1fr", gap: "14px"}}>
+                       <div>Fail:</div> 
+                       <input style={{marginTop: "1px"}} type="checkbox"></input> 
+                       <input style={{marginTop: "1px"}} type="checkbox"></input> 
+                       <input style={{marginTop: "1px"}} type="checkbox"></input> 
+                       <div>Save:</div> 
+                       <input style={{marginTop: "1px"}} type="checkbox"></input> 
+                       <input style={{marginTop: "1px"}} type="checkbox"></input> 
+                       <input style={{marginTop: "1px"}} type="checkbox"></input> 
+                    </div>
+            </article>
+            }
         </>
 
     )
--- src/components/initiative/initiative.js
+++ src/components/initiative/initiative.js
@@ -1,44 +1,44 @@
 import React, { useState } from 'react';
 import './initiative.css';
 import Plaque from '../Plaque/Plaque';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Initiative() {
 
     const [participants, setParticipants] = useState({
-        "30" : [],
-        "29" : [],
-        "28" : [],
-        "27" : [],
-        "26" : [],
-        "25" : [],
-        "24" : [],
-        "23" : [],
-        "22" : [],
-        "21" : [],
-        "20" : [],
-        "19" : [],
-        "18" : [],
-        "17" : [],
-        "16" : [],
-        "15" : [],
-        "14" : [],
-        "13" : [],
-        "12" : [],
-        "11" : [],
-        "10" : [],
-        "9" : [],
-        "8" : [],
-        "7" : [],
-        "6" : [],
-        "5" : [],
-        "4" : [],
-        "3" : [],
-        "2" : [],
-        "1" : [],
-        "0" : [],
-        "-1" : [],
-        "-2" : []
+        "30": [],
+        "29": [],
+        "28": [],
+        "27": [],
+        "26": [],
+        "25": [],
+        "24": [],
+        "23": [],
+        "22": [],
+        "21": [],
+        "20": [],
+        "19": [],
+        "18": [],
+        "17": [],
+        "16": [],
+        "15": [],
+        "14": [],
+        "13": [],
+        "12": [],
+        "11": [],
+        "10": [],
+        "9": [],
+        "8": [],
+        "7": [],
+        "6": [],
+        "5": [],
+        "4": [],
+        "3": [],
+        "2": [],
+        "1": [],
+        "0": [],
+        "-1": [],
+        "-2": []
     })
 
     const [inputName, setInputName] = useState("")
@@ -48,21 +48,49 @@ export default function Initiative() {
 
 
     function addParticipant(nme, ac, hp, init) {
-        let newParticipants = {...participants}
-        newParticipants[init].push({"name": nme, "ac": ac, "hp": hp, "init": init, "id": uuidv4()})
-        setParticipants(newParticipants)
+        let newParticipants = { ...participants }
+        newParticipants[init].push({
+            "name": nme,
+            "ac": ac,
+            "hp": hp,
+            "init": init,
+            "id": uuidv4(),
+            "conditions": {
+                "frightened": false,
+                "poisoned": false,
+                "charmed": false,
+                "grappled": false,
+                "paralyzed": false,
+                "petrified": false,
+                "blind": false,
+                "prone": false
+            }
+        })
+        setParticipants({...newParticipants})
     }
 
-    function removeParticipant(id, init){
+    function removeParticipant(id, init) {
         let newParts = { ...participants }
         newParts[init] = newParts[init].filter(p => p.id !== id)
-        setParticipants(newParts)
+        setParticipants({...newParts})
+        console.log(participants)
     }
 
-    function rndr(){
+    function updateParticipant(prt){
+        let newParts = {...participants}
+        let row = newParts[prt.init]
+        for(let e of row){
+            if(e.id === prt.id){
+                e = prt
+            }
+        }
+        setParticipants({...newParts})
+    }
+
+    function rndr() {
         let outPut = []
-        for(let row in participants){
-            if(participants[row].length > 0){
+        for (let row in participants) {
+            if (participants[row].length > 0) {
                 outPut.push(participants[row])
             }
         }
@@ -72,16 +100,16 @@ export default function Initiative() {
 
     return (
         <>
-            <div className='container' style={{marginBottom:"2em"}}>
+            <div className='container' style={{ marginBottom: "2em" }}>
                 <article id="participantInput">
+                    <input id="nameInput" type={"text"} placeholder={"name"} onChange={(e) => setInputName(e.target.value)} ></input>
                     <input type={"text"} placeholder={"hp"} onChange={(e) => setInputHp(e.target.value)} ></input>
                     <input type={"text"} placeholder={"ac"} onChange={(e) => setInputAc(e.target.value)} ></input>
                     <input type={"text"} placeholder={"init"} onChange={(e) => setInputInit(e.target.value)} ></input>
-                    <input id="nameInput" type={"text"} placeholder={"name"} onChange={(e) => setInputName(e.target.value)} ></input>
                     <button onClick={() => addParticipant(inputName, inputAc, inputHp, inputInit)}> Add </button>
                 </article>
                 <div className='participantGrid'>
-                    {rndr().map(r => <div className='participantRow'>{r.map(p => <Plaque name={p.name} ac={p.ac}  hp={p.hp}  init={p.init} id={p.id} removeFunc={() => removeParticipant(p.id, p.init)}/>)}</div>)}
+                    {rndr().map(r => <div className='participantRow'>{r.map(p => <Plaque name={p.name} ac={p.ac} hp={p.hp} init={p.init} id={p.id} conditions={p.conditions} removeFunc={() => removeParticipant(p.id, p.init)} updateFunc={updateParticipant} />)}</div>)}
                 </div>
             </div>
         </>
